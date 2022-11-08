683f2acedee5a4e8c3f50a19c91af63cd5dd19a6
paola <paola.diazs@anahuac.mx>
2022-11-07 18:05:39 -0600

    cambios 1

--- node/database/db.js
+++ node/database/db.js
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize'
 
 const db = new Sequelize('pasteleria_piccolo','root','#Ginebraencasa23',{
-    host:'127.0.0.1',
+    host:'10.147.18.132',
     dialect: 'mysql',
     define: {
         timestamps: false,
